bf7ec17823bb11ff7a05c7f49a679b58203a4d94
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:56:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,10 +17,10 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
+  hash = load_library(path)
+  result = hash["get_emoticon"][emoticon]
   if result == nil
-    result = "Sorry, that emoticon was not found" 
+    result = "Sorry, emoticon not found!" 
   end
   result
 end
