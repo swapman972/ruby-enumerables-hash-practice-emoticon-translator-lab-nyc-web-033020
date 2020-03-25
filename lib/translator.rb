253458f38db9ca053d5db8fb382aa03a9f86627a
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:54:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ require 'pry'
 
 def load_library(path)
   emoticons = YAML.load_file(path)
-  emoticon_hash = Hash.new
+  emoticon_hash = {}
 
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
+  emoticon_hash["get_emoticon"] = {}
+  emoticon_hash["get_meaning"] = {}
 
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
